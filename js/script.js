5ad5759ec4a6a1c611503592779c10578b4cba23
Athos Balmant <athos.kyoto@gmail.com>
2023-01-29 19:32:44 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,193 @@
+$(".owl-carousel").owlCarousel({
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  loop: true,
+  margin: 50,
+  responsiveClass: true,
+  nav: true,
+  loop: true,
+  stagePadding: 100,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    568: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+$(document).ready(function () {
+  $(".popup-image").magnificPopup({
+    disableOn: 320,
+    type: "image",
+    mainClass: "mfp-fade",
+    removalDelay: 160,
+    preloader: true,
+    fixedContentPos: true,
+  });
+});
+$(".item").magnificPopup({
+  delegate: "a",
+});
+
+$(document).ready(function () {
+  $(".popup-video").magnificPopup({
+    disableOn: 320,
+    type: "iframe",
+    mainClass: "mfp-fade",
+    removalDelay: 160,
+    preloader: true,
+    fixedContentPos: true,
+  });
+});
+$(".item").magnificPopup({
+  delegate: "a",
+});
+
+function moveToSelected(element) {
+  if (element == "next") {
+    var selected = $(".selected").next();
+  } else if (element == "prev") {
+    var selected = $(".selected").prev();
+  } else {
+    var selected = element;
+  }
+
+  var next = $(selected).next();
+  var prev = $(selected).prev();
+  var prevSecond = $(prev).prev();
+  var nextSecond = $(next).next();
+
+  $(selected).removeClass().addClass("selected");
+
+  $(prev).removeClass().addClass("prev");
+  $(next).removeClass().addClass("next");
+
+  $(nextSecond).removeClass().addClass("nextRightSecond");
+  $(prevSecond).removeClass().addClass("prevLeftSecond");
+
+  $(nextSecond).nextAll().removeClass().addClass("hideRight");
+  $(prevSecond).prevAll().removeClass().addClass("hideLeft");
+}
+
+// Eventos teclado
+$(document).keydown(function (e) {
+  switch (e.which) {
+    case 37: // left
+      moveToSelected("prev");
+      break;
+
+    case 39: // right
+      moveToSelected("next");
+      break;
+
+    default:
+      return;
+  }
+  e.preventDefault();
+});
+
+$("#carousel div").click(function () {
+  moveToSelected($(this));
+});
+
+$("#prev").click(function () {
+  moveToSelected("prev");
+});
+
+$("#next").click(function () {
+  moveToSelected("next");
+});
+
+class StickyNavigation {
+  constructor() {
+    this.currentId = null;
+    this.currentTab = null;
+    this.tabContainerHeight = 70;
+    let self = this;
+    $(".et-hero-tab").click(function () {
+      self.onTabClick(event, $(this));
+    });
+    $(window).scroll(() => {
+      this.onScroll();
+    });
+    $(window).resize(() => {
+      this.onResize();
+    });
+  }
+
+  onTabClick(event, element) {
+    event.preventDefault();
+    let scrollTop =
+      $(element.attr("href")).offset().top - this.tabContainerHeight + 1;
+    $("html, body").animate({ scrollTop: scrollTop }, 600);
+  }
+
+  onScroll() {
+    this.checkTabContainerPosition();
+    this.findCurrentTabSelector();
+  }
+
+  onResize() {
+    if (this.currentId) {
+      this.setSliderCss();
+    }
+  }
+
+  checkTabContainerPosition() {
+    let offset =
+      $(".et-hero-tabs").offset().top +
+      $(".et-hero-tabs").height() -
+      this.tabContainerHeight;
+    if ($(window).scrollTop() > offset) {
+      $(".et-hero-tabs-container").addClass("et-hero-tabs-container--top");
+    } else {
+      $(".et-hero-tabs-container").removeClass("et-hero-tabs-container--top");
+    }
+  }
+
+  findCurrentTabSelector(element) {
+    let newCurrentId;
+    let newCurrentTab;
+    let self = this;
+    $(".et-hero-tab").each(function () {
+      let id = $(this).attr("href");
+      let offsetTop = $(id).offset().top - self.tabContainerHeight;
+      let offsetBottom =
+        $(id).offset().top + $(id).height() - self.tabContainerHeight;
+      if (
+        $(window).scrollTop() > offsetTop &&
+        $(window).scrollTop() < offsetBottom
+      ) {
+        newCurrentId = id;
+        newCurrentTab = $(this);
+      }
+    });
+    if (this.currentId != newCurrentId || this.currentId === null) {
+      this.currentId = newCurrentId;
+      this.currentTab = newCurrentTab;
+      this.setSliderCss();
+    }
+  }
+
+  setSliderCss() {
+    let width = 0;
+    let left = 0;
+    if (this.currentTab) {
+      width = this.currentTab.css("width");
+      left = this.currentTab.offset().left;
+    }
+    $(".et-hero-tab-slider").css("width", width);
+    $(".et-hero-tab-slider").css("left", left);
+  }
+}
+
+new StickyNavigation();
